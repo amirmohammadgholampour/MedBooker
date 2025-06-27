d9373d801fd9bdf5a46a7590456235dadbe3dbec
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-06-27 23:23:45 +0330

    Add apps to settings.py on Installed apps list

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'apps', 
 ]
 
 ROOT_URLCONF = 'config.urls'
