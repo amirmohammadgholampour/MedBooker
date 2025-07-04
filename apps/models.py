3d786d4ee015eeb886773afd6577e885fef6b3ec
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-07-04 05:43:14 +0330

    Import appointment model in main models.py

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,4 @@
 from apps.main_models.user_models import User, UserManager
 from apps.main_models.doctor_models import Doctor
 from apps.main_models.doctor_schedule_models import DoctorSchedule
+from apps.main_models.appointment_models import Appointment 
