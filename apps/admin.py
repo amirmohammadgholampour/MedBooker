fa8e1157a057283512c21ce454f6f427d4c96e6e
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-07-04 05:45:50 +0330

    Import appointment admin in main admin.py

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,4 @@
 from apps.main_admins.user_admin import UserAdmin
 from apps.main_admins.doctor_admin import DoctorAdmin 
 from apps.main_admins.doctor_schedule_admin import DoctorScheduleAdmin 
+from apps.main_admins.appointment_admin import AppointmentAdmin 
