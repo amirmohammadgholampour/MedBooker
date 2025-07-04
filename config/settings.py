7549293187add7af890eca0ad4da0b1022ab76c4
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-07-04 06:19:30 +0330

    Install DRF and config it

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     'apps',
     'grappelli',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +58,17 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 TEMPLATES = [
     {
